05cccf11a8dfa45ffdee7b033872cee9b2a23270
Vicente Velasco <velasco.josevicente@gmail.com>
2022-01-14 21:07:55 -0500

    Se finaliza header parcialmente

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import '../estilos/header.scss'
 import logo from '../media/logo.png'
+import {Link} from 'react-router-dom'
 
 const Header = () => {
     return (
         <div className='header'>
-            <img className='logo' src={logo} alt="logo_concurso" id='logo_header' />
-            <span className='title'><b>Este es el header</b></span>
+            <Link to='/'><img className='logo' src={logo} alt="logo_concurso" id='logo_header' /> </Link>
+            {/* Agregar funcionalidad para que el titulo tome el nombre del modulo actual */}
+            <span className='title '><b>Este es el header</b></span>
         </div>
     )
 }
--- src/paginas/Ingreso.jsx
+++ src/paginas/Ingreso.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { useNavigate } from 'react-router';
 import useFormData from '../hooks/useFormData'
 import Header from '../components/Header.jsx'
+import logo from '../media/logo.png'
+import '../estilos/contenido.scss'
 
 const Ingreso = () => {
 
@@ -10,18 +12,23 @@ const Ingreso = () => {
     const navigate = useNavigate();
     return (
         <div>
-            <Header/>
+            <Header />
+            <div className='contenedor '>
+                <div className='borde registro'>
 
-            <div className='borde'>
-                <h3>Datos del jugador</h3>
+                    <img className='logo borde' src={logo} alt="logo_registro" id='logo_registro' />
+
+                    <div className='borde'>
+                        <h3>Datos del jugador</h3>
+                        <form className='borde'>
+                            <input className='mb-3' required type='text' placeholder='Tu numero de identificación' name='identificacion' />
+                            <input className='mb-3' required type='text' placeholder='Tus nombres' name='nombres' />
+                            <input className='mb-3' required type='text' placeholder='Tus apellidos' name='apellidos' />
+                            <button type='submit'> Agregar usuario</button>
+                        </form>
+                    </div>
+                </div>
             </div>
-            <form className='borde abs-center display-flex mb-3'>
-                
-                <input className='mb-3' required type='text' placeholder='Tu numero de identificación' name='identificacion'/>
-                <input className='mb-3' required type='text' placeholder='Tus nombres' name='nombres'/>
-                <input className='mb-3' required type='text' placeholder='Tus apellidos' name='apellidos'/>
-                <button type='submit'> Agregar usuario</button>
-            </form>
         </div>
     )
 }
